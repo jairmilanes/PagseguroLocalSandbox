48a5e5695f9fdf8c0f674620642e4b106118ac60
Jair <web@layoutz.com.br>
2014-05-13 21:00:55 -0300

    No logo and file name conventions to mimic PagSeguros Apis

--- view/transactions_table.php
+++ view/transactions_table.php
@@ -1,3 +1,30 @@
+<?php
+/*
+ *************************************************************************
+* The MIT License (MIT)
+*
+* Copyright (c) [2014] [Jair Milanes Junior]
+
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+************************************************************************
+*/
+?>
 <?php $transactions = ResponseHelper::getInstance()->getData('transactions');?>
 <table id="transactions_table" class="table table-striped table-bordered table-hover">
  	<tr>
@@ -9,20 +36,20 @@
  		<th>#</th>
  		<th>#</th>
  	</tr>
- 	<?php 
+ 	<?php
  	
  	if( count((array)$transactions) == 0 ){ ?>
 	<tr class="transaction empty">
 	 	<td colspan="7">
 	 		<span class="glyphicon glyphicon-warning-sign"></span>
 	 		<h3>Nenhuma transação encontrada!</h3>
-	 		<p>Se está tendo problemas na configuração da SandBox, por favor veja a 
+	 		<p>Se está tendo problemas na configuração da SandBox, por favor veja a
 	 		documentação no <a href="https://github.com/layoutzweb/PagseguroLocalSandbox">github</a>,
 	 		caso precise de ajuda visite nosso forum.</p>
 	 	</td>
 	</tr>
 <?php 	} else {
-	 	foreach( $transactions as $trans ){ 
+	 	foreach( $transactions as $trans ){
 	 			$xml = simplexml_load_string($trans->xml);?>
 	 		<tr class="transaction">
 	 			<td><?php echo $xml->code; ?></td>
@@ -48,6 +75,6 @@
 	 				</a>
 	 			</td>
 	 		</tr>
-	 	<?php } 
+	 	<?php }
 	 }?>
 </table>
